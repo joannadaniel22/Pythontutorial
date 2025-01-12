3d12db1bc6c3c12791a814e573668d171f75ccd6
joannadaniel22 <156598901+joannadaniel22@users.noreply.github.com>
2025-01-12 19:58:12 +0530

    Update income.py

--- income.py
+++ income.py
@@ -1,2 +1,12 @@
-income=int(input("Enter income"))
+
+income=int(input("enter income "))
+if income<=250000:
+    tax=0
+elif income > 250000 and income <= 500000:
+    tax=(income-250000)*0.05
+elif income>500000 and income <=1000000:
+    tax=(income-500000)*0.2 + 250000*0.05
+else:
+    tax=(income-1000000)*0.3 +500000*0.2+250000*0.05
+print("The income tax for",income,"is",tax)
 
